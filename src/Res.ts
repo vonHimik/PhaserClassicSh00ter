5777a1eaeb61854f8c486fa5dd8b7961334d8ee5
vonHimik <assassiny1244@gmail.com>
2019-06-28 21:10:27 +0200

    new

---
+++ src/Res.ts
@@ -0,0 +1,20 @@
+  export class Res extends Phaser.Scene {
+
+      constructor(){
+          super("Res");
+      }
+
+      preload() {
+          console.log("Res.preload()");
+          this.load.setBaseURL("https://cdn.jsdelivr.net/gh/kefik/kenney/Shooter/");
+          this.load.image("playership1", "playerShip1_blue.png");
+          this.load.image("bullet", "lasers/laserBlue01.png");
+          this.load.image("enemy", "enemies/enemyBlack1.png");
+      }
+
+      create() {
+        console.log("R.create()");
+        this.scene.start("Play");
+      }
+
+  }
