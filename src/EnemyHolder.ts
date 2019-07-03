dea9db177334b9b31f3aab8f9ff798f4c627ff48
vonHimik <assassiny1244@gmail.com>
2019-07-03 20:15:11 +0200

    EnemyHolder fix

--- src/EnemyHolder.ts
+++ src/EnemyHolder.ts
@@ -1,20 +1,18 @@
 export class EnemyHolder 
 {
-   ///// {Honza}
-   // Enemy variables to customise them and their beavior
+  // {Honza}
+  // Enemy variables to customise them and their beavior
 
-   // Name of the sprite
-   spriteName: string = null;
+  // Name of the sprite
+  spriteName:string = null;
 
-   // Update function
-   updateFunction:(x: number, y:number)=>void = null;
+  // Update function
+  updateFunction:(x:number, y:number)=>void = null;
 
-   /////
-
-   constructor( _name: string, _updateFunction:(x: number, y:number)=>void ) 
-   {
-       this.spriteName = _name;
-       this.updateFunction = _updateFunction;
+  constructor (_name:string, _updateFunction:(x:number, y:number)=>void) 
+  {
+    this.spriteName = _name;
+    this.updateFunction = _updateFunction;
    }
 
 }
