5cf16116e797aa082c667311e29e6b2d2d1d020f
vonHimik <assassiny1244@gmail.com>
2019-07-03 17:24:56 +0200

    Background fix

--- src/Background.ts
+++ src/Background.ts
@@ -1,41 +1,42 @@
 export class Background extends Phaser.Physics.Arcade.Sprite 
 {
-        speed: number;
+  speed:number;
 
-       constructor( scene: Phaser.Scene ) 
-       {
-           super(scene, 0, 0, "background");
-           this.speed = Phaser.Math.GetSpeed(Math.random() * 30 + 20, 1);         
-       }
-        launch(x: number, y: number) : Background 
-        {
-           Phaser.Physics.Arcade.Sprite.call(this, this.scene, 0, 0, 'background');
-          
-           this.setScale(this.speed * 10, this.speed * 10);
+  // Конструктор объекта Фон.
+  constructor (scene: Phaser.Scene) 
+  {
+    // Настраиваем место появления (текущая сцена, координаты х и у), текстуру.
+    super (scene, 0, 0, "background");
 
-           this.scene.physics.add.existing(this);
-           this.body.height *= 0.3;
-           this.body.width *= 0.3;
-           this.alpha *= (10 * this.speed);
+    // Настраиваем скорость (пикселей за фрейм).
+    this.speed = Phaser.Math.GetSpeed (Math.random() * 30 + 20, 1);         
+  }
 
-          this.setTintFill(0x333399);
+  // Метод для запуска фона, настройка стартовых параметров.
+  launch (x:number, y:number) : Background 
+  {
+    Phaser.Physics.Arcade.Sprite.call (this, this.scene, 0, 0, 'background');
+    this.setScale (this.speed * 10, this.speed * 10);
+    this.scene.physics.add.existing (this);
+    this.body.height *= 0.3;
+    this.body.width *= 0.3;
+    this.alpha *= (10 * this.speed);
+    this.setTintFill (0x333399);
+    this.setPosition (x, y);
+    this.setActive (true);
+    this.setVisible (true);
+    return this;
+  }
 
-           this.setPosition(x, y);
-
-           this.setActive(true);
-           this.setVisible(true);
-
-           return this;
-       }
-
-       update(time: number, delta: number) 
-       {
-           this.y += this.speed * delta;
-
-           if (this.y > Number(this.scene.game.config.height) + 50)
-           {
-               this.setActive(false);
-               this.setVisible(false);
-           }
-       }
+  // Метод для обновления состояния фо (перемещение, вылет за сцену).
+  update (time:number, delta:number) 
+  {
+    this.y += this.speed * delta;
+    
+    if (this.y > Number (this.scene.game.config.height) + 50)
+    {
+      this.setActive  (false);
+      this.setVisible (false);
     }
+  }
+}
